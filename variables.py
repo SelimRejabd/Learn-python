7c46c96f51a43fe4ccd3c08eeb100798ca786bb3
SelimRejabd <48088144+SelimRejabd@users.noreply.github.com>
2021-06-05 13:36:34 +0600

    variables in python

---
+++ variables.py
@@ -0,0 +1,33 @@
+# variables = a container for a value.
+# no need declare data type like c/c++
+
+
+# String data type
+
+first_name = "Selim"
+last_name = "Reja"
+full_name = first_name + " " + last_name
+
+print((full_name))
+print(type(first_name))
+
+# Integer data type
+age = 23
+age += 1
+print(+(age))
+print(type(age))
+
+# float data type
+
+salary = 120000.50
+print(+(salary))
+print(type(salary))
+
+# add string and integer data type/ string cast
+print("Your salary is " + str(salary))
+
+# bolean data type
+
+human = False
+print(human)
+print(type(human))
