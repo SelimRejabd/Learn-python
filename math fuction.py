ad1dd8ca81e16664522b45d8d3ecff7195ab39b7
SelimRejabd <48088144+SelimRejabd@users.noreply.github.com>
2021-06-05 13:44:43 +0600

    some useful math function

---
+++ math fuction.py
@@ -0,0 +1,15 @@
+import math
+
+# some useful math function
+
+pi = 3.14
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi))
+print(pow(pi, 2))
+print(math.sqrt(49))
+
+x, y, z = 5, 9, 2
+print(max(x, y, z))
+print(min(x, y, z))
