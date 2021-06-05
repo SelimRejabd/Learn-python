9ad7620fcb8498377ff61f489990887c1dc5f5a4
SelimRejabd <48088144+SelimRejabd@users.noreply.github.com>
2021-06-05 13:35:56 +0600

    Create multiple assignment.py

---
+++ multiple assignment.py
@@ -0,0 +1,14 @@
+# Learn-python
+Config files for my GitHub profile.
+
+# multiple assignment allows us to assign multiple variablse at the same time in one line of code
+
+#name = "Reja"
+#age = 23
+#attractive = True
+
+name, age, attractive = "Reja", 23, True
+
+print(name)
+print(age)
+print(attractive)
