ebd941e588343440f41f50c5ccee8396fa65777b
SelimRejabd <48088144+SelimRejabd@users.noreply.github.com>
2021-06-05 13:39:14 +0600

    Type cast in python

---
+++ type casting.py
@@ -0,0 +1,20 @@
+# type casting = convert the data type of a value to another data type
+x = 1  # int
+y = 2.0  # float
+z = "3"  # str
+
+x = str(x)
+y = int(y)
+z = int(z)
+
+print(type(x))
+
+print(type(y))
+
+print(type(z))
+
+s = "3"
+print(s*3)
+print(z*3)
+
+print("Value of S is: " + str(s))
