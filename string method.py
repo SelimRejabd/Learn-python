85a3b61aca5cd8fa0d6aec1af0bcdcdcf2a8822d
SelimRejabd <48088144+SelimRejabd@users.noreply.github.com>
2021-06-05 13:37:41 +0600

    Some useful string method in python

---
+++ string method.py
@@ -0,0 +1,32 @@
+# few useful method for string
+name = "reja"
+
+# lenth of string
+print(len(name))
+
+# finding a charecter
+print(name.find("a"))
+
+# Capitalize "reja" to "Reja"
+print(name.capitalize())
+
+# uppercase of a string
+print(name.upper())
+
+# lowecase of a string
+print(name.lower())
+
+# is string digit or not
+print(name.isdigit())
+
+# is string alphabatic or not
+print(name.isalpha())
+
+# count  charecter in string
+print(name.count("e"))
+
+# replace charecter
+print(name.replace("j", "z"))
+
+# string print multiple time
+print(name*2)
