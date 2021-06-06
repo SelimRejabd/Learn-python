6e340292e4eb88334dd2b5996eacae2ed672d0d6
SelimRejabd <48088144+SelimRejabd@users.noreply.github.com>
2021-06-06 09:23:59 +0600

    2D list (Matrix) in python

---
+++ 2D list.py
@@ -0,0 +1,15 @@
+# 2D list in python
+
+drinks = ["coffe", "tea", "soda"]
+dinner = ["pizza", "burger", "hot dog"]
+dessert = ["cake", "ice cream"]
+
+food = [drinks, dinner, dessert]
+# print(food[0])
+# print(food[1])
+# print(food[2])
+
+for x in food:
+    print(x)
+
+print(food[1][1])
