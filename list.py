7f9ef7dedeba6a81416a9a974ce53160315130fb
SelimRejabd <48088144+SelimRejabd@users.noreply.github.com>
2021-06-06 09:23:18 +0600

    list in python

---
+++ list.py
@@ -0,0 +1,12 @@
+# list in python
+
+food = ["pizza", "burger", "ice cream"]
+#food[0] = "sushi"
+# food.append("cocolate")
+# food.remove("pizza")
+# food.pop()
+#food.insert(0, "cake")
+# food.sort()
+food.clear()
+for i in food:
+    print(i)
