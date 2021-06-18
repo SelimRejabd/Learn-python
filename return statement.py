275d6c245f31b5265e76e192624ac1d95b5c2cd8
SelimRejabd <48088144+SelimRejabd@users.noreply.github.com>
2021-06-18 08:15:00 +0600

    return value from function

---
+++ return statement.py
@@ -0,0 +1,9 @@
+# return statement = Functions send python values/objects back to the caller.
+#                    These values/objects are known as the fubnction's return avlue
+
+def multiply(number1, number2):
+    result = number1*number2
+    return result
+
+
+print(multiply(6, 8))
