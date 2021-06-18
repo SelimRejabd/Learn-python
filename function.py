501cebcfa3342f67956647fae66d10ace4775b45
SelimRejabd <48088144+SelimRejabd@users.noreply.github.com>
2021-06-18 08:14:13 +0600

    function in python

---
+++ function.py
@@ -0,0 +1,10 @@
+# function is a block of code which is execute only when it is called
+
+def hello(name, age):
+    print("Hello! " + name)
+    print("You are " + str(age) + " Years old")
+    print("Have a nice day!")
+
+
+my_name = "Selim Raja"
+hello(my_name, 23)
